8392d54602e9f17237107720687d6db6772e31e8
Pull-eckermann <eric7880@gmail.com>
2023-01-17 23:14:58 -0300

    .

--- list_interfaces.sh
+++ list_interfaces.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-#Guarda em variáveis os valores separados para as interfaces
-INTF= ifconfig | grep mtu | cut -d: -f1 | grep -v "lo"
-STATE= ifconfig | grep mtu | grep -v lo | cut -d"<" -f2 | cut -d"," -f1
-MTU= ifconfig | grep mtu | grep -v lo | cut -d" " -f5
-IPS= ifconfig | grep " inet " | grep -v "127.0.0.1" | sed 's/        //g' | cut -d" " -f2
-MACS= ifconfig | grep ether | sed 's/        //g' | cut -d" " -f2
+#Guarda em variáveis os valores separados das características das interfaces
+for a in `ifconfig | grep mtu | grep -v "LOOPBACK" | cut -d: -f1`; do
+  intf+=("$a"); done
 
-echo -n $INTF
-echo -n $STATE
-echo -n $MTU
-echo -n $IPS
-echo -n $MACS
+for a in `ifconfig | grep mtu | grep -v "LOOPBACK" | cut -d"<" -f2 | cut -d"," -f1`; do
+  states+=("$a"); done
+
+for a in `ifconfig | grep mtu | grep -v "LOOPBACK" | cut -d" " -f5`; do
+  mtu+=("$a"); done
+
+for a in `ifconfig | grep " inet " | grep -v "127.0.0.1" | sed 's/        //g' | cut -d" " -f2`; do
+  ips+=("$a"); done
+
+for a in `ifconfig | grep ether | sed 's/        //g' | cut -d" " -f2`; do
+  macs+=("$a"); done
+
+aux=${#intf[@]} #Tamanho dos arrays
+tam=$(($aux-1)) #Tamanho -1 para acessar indice por indice
+for i in $(seq 0 $tam); do
+  echo "Interface: "${intf[$i]}
+  echo "Estado: "${states[$i]}
+  echo "MTU: "${mtu[$i]}
+  echo "IP: "${ips[$i]}
+  echo "MAC: "${macs[$i]}
+  echo #Propósito de pular linha
+done
